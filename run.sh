6f434d61eb264d2c75c0d0f24e2ba4c1d3d9b979
Brendan Gordon <brendan.gordon@digital.gov.au>
2019-05-10 18:14:46 +1000

    Run in parallel

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+: "${CF_API:?Need to set CF_API}"
+: "${CF_PASSWORD:?Need to set CF_PASSWORD}"
+
 CONFIG=/tmp/config.json
 
 mkdir -p /go/src/github.com/cloudfoundry
@@ -9,37 +12,14 @@ cd /go/src/github.com/cloudfoundry
 git clone https://github.com/cloudfoundry/cf-smoke-tests
 cd cf-smoke-tests
 
-# STAGING
-APPS_DOMAIN="${CF_API_STAGING/https:\/\/api.system/apps}"
+APPS_DOMAIN="${CF_API/https:\/\/api.system/apps}"
 cat << EOF > ${CONFIG}
 {
     "suite_name": "CF_SMOKE_TESTS",
-    "api": "${CF_API_STAGING}",
+    "api": "${CF_API}",
     "apps_domain": "${APPS_DOMAIN}",
     "user": "${CF_USER}",
-    "password": "${CF_PASSWORD_STAGING}",
-    "cleanup": true,
-    "logging_app": "",
-    "runtime_app": "",
-    "enable_windows_tests": false,
-    "enable_isolation_segment_tests": false,
-    "use_existing_org": true,
-    "use_existing_space": true,
-    "org": "${CF_ORG}",
-    "space": "${CF_SPACE}"
-}
-EOF
-CONFIG="${CONFIG}" ./bin/test
-
-# PROD
-APPS_DOMAIN="${CF_API_PROD/https:\/\/api.system/apps}"
-cat << EOF > ${CONFIG}
-{
-    "suite_name": "CF_SMOKE_TESTS",
-    "api": "${CF_API_PROD}",
-    "apps_domain": "${APPS_DOMAIN}",
-    "user": "${CF_USER}",
-    "password": "${CF_PASSWORD_PROD}",
+    "password": "${CF_PASSWORD}",
     "cleanup": true,
     "logging_app": "",
     "runtime_app": "",
